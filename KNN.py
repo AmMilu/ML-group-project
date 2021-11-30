95e45b3e8f755a1ebab6c8bd69f6c08e38441c61
AmMilu <hehu@tcd.ie>
2021-11-30 16:26:29 +0000

    add polynomial feature to Lasso and add Gaussian weights to KNN

--- KNN.py
+++ KNN.py
@@ -8,6 +8,7 @@ def output_evaluate(target_value, predict_value):
     print('Mean squared error:', mean_square_error)
     print('Root mean squared error:', root_mean_squared_error)
     print('Coefficient of determination', coefficient_of_determination)
+    print('------------------------------------------------------------')
 
 #read data
 import numpy as np
@@ -25,9 +26,123 @@ y = df.iloc[:,1]/1000 #rent is mostly four digits, to make value smaller and eas
 from sklearn.model_selection import train_test_split
 Xtrain, Xtest, ytrain, ytest = train_test_split(X,y,test_size=0.1,shuffle=True)
 
+def gaussian_kernel1(distances):
+    weights = np.exp(-1*(distances**2))
+    return weights/np.sum(weights)
+
+def gaussian_kernel5(distances):
+    weights = np.exp(-5*(distances**2))
+    return weights/np.sum(weights)
+
+def gaussian_kernel10(distances):
+    weights = np.exp(-10*(distances**2))
+    return weights/np.sum(weights)
+
+def gaussian_kernel50(distances):
+    weights = np.exp(-50*(distances**2))
+    return weights/np.sum(weights)
+
+def gaussian_kernel100(distances):
+    weights = np.exp(-100*(distances**2))
+    return weights/np.sum(weights)
+
+def gaussian_kernel500(distances):
+    weights = np.exp(-500*(distances**2))
+    return weights/np.sum(weights)
+
+def gaussian_kernel1000(distances):
+    weights = np.exp(-1000*(distances**2))
+    return weights/np.sum(weights)
+
+#def gaussian_kernel10000(distances):
+#    weights = np.exp(-10000*(distances**2))
+#    return weights/np.sum(weights)
+
+gaussian_kernel = [gaussian_kernel1, gaussian_kernel5, gaussian_kernel10, gaussian_kernel50, gaussian_kernel100, gaussian_kernel500, gaussian_kernel1000]
+g = [1,5,10,50,100,500,1000]
+
 neighbors = [1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15]
 from sklearn.model_selection import KFold
 kf = KFold(n_splits=5,shuffle=True)
+uniform = False
+#weights = gaussian
+for i in range(len(gaussian_kernel)):
+    mean_error_mse = []
+    std_error_mse = []
+    mean_error_r2 = []
+    std_error_r2 = []
+    for ni in neighbors:
+        from sklearn.neighbors import KNeighborsRegressor
+        model = KNeighborsRegressor(n_neighbors=ni, weights=gaussian_kernel[i])
+        temp_mse = []
+        temp_r2 = []
+        for train, test in kf.split(Xtrain,ytrain):
+            model.fit(Xtrain[train], ytrain.to_numpy()[train])
+            ypred = model.predict(Xtrain[test])
+            from sklearn.metrics import mean_squared_error,r2_score
+            temp_mse.append(mean_squared_error(ytrain.to_numpy()[test],ypred))
+            temp_r2.append(r2_score(ytrain.to_numpy()[test],ypred))
+        mean_error_mse.append(np.array(temp_mse).mean())
+        std_error_mse.append(np.array(temp_mse).std())
+        mean_error_r2.append(np.array(temp_r2).mean())
+        std_error_r2.append(np.array(temp_r2).std())
+    import matplotlib.pyplot as plt
+    plt.errorbar(neighbors, mean_error_mse, yerr=std_error_mse, linewidth=3, label="Gamma = "+str(g[i]))
+plt.ylabel('Mean square error')
+plt.xlabel('n')
+plt.legend()
+plt.show()
+
+mean_error_mse = []
+std_error_mse = []
+mean_error_r2 = []
+std_error_r2 = []
+for w in gaussian_kernel:
+    from sklearn.neighbors import KNeighborsRegressor
+    model = KNeighborsRegressor(n_neighbors=6, weights=w)
+    temp_mse = []
+    temp_r2 = []
+    for train, test in kf.split(Xtrain,ytrain):
+        model.fit(Xtrain[train], ytrain.to_numpy()[train])
+        ypred = model.predict(Xtrain[test])
+        from sklearn.metrics import mean_squared_error,r2_score
+        temp_mse.append(mean_squared_error(ytrain.to_numpy()[test],ypred))
+        temp_r2.append(r2_score(ytrain.to_numpy()[test],ypred))
+    mean_error_mse.append(np.array(temp_mse).mean())
+    std_error_mse.append(np.array(temp_mse).std())
+    mean_error_r2.append(np.array(temp_r2).mean())
+    std_error_r2.append(np.array(temp_r2).std())
+import matplotlib.pyplot as plt
+plt.errorbar(g, mean_error_mse, yerr=std_error_mse, linewidth=3)
+plt.ylabel('Mean square error')
+plt.xlabel('n')
+plt.show()
+
+plt.errorbar(g, mean_error_r2, yerr=std_error_r2, linewidth=3)
+plt.ylabel('R2 score')
+plt.xlabel('n')
+plt.show()
+
+n = 6
+w = gaussian_kernel50
+model = KNeighborsRegressor(n_neighbors=n, weights = w)
+model.fit(Xtrain,ytrain)
+ypred = model.predict(Xtest)
+
+#on the 45 degree line, the predict value is same as the original value
+#above the line, predicted value is larger and below the line, predicted value is smaller
+plt.scatter(ytest, ypred)
+plt.xlim(1,7)
+plt.xlabel("ytest")
+plt.ylim(1,7)
+plt.ylabel("ypred")
+a=np.array([1,2,3,4,5,6,7])
+plt.plot(a,a,'r')
+plt.show()
+print('n = 6, Gaussian weight = 50:')
+output_evaluate(ytest,ypred)
+
+#weights = uniform 
 mean_error_mse = []
 std_error_mse = []
 mean_error_r2 = []
@@ -73,7 +188,30 @@ plt.ylabel("ypred")
 a=np.array([1,2,3,4,5,6,7])
 plt.plot(a,a,'r')
 plt.show()
+print("n=4, weights = uniform:")
+output_evaluate(ytest,ypred)
 
+#Mean absolute error: 0.12509324324324328
+#Mean squared error: 0.045088912837837856
+#Root mean squared error: 0.2123415005076442
+#Coefficient of determination 0.9379890126640795
+
+n = 6
+model = KNeighborsRegressor(n_neighbors=n, weights = "uniform")
+model.fit(Xtrain,ytrain)
+ypred = model.predict(Xtest)
+
+#on the 45 degree line, the predict value is same as the original value
+#above the line, predicted value is larger and below the line, predicted value is smaller
+plt.scatter(ytest, ypred)
+plt.xlim(1,7)
+plt.xlabel("ytest")
+plt.ylim(1,7)
+plt.ylabel("ypred")
+a=np.array([1,2,3,4,5,6,7])
+plt.plot(a,a,'r')
+plt.show()
+print("n=6, weights = uniform:")
 output_evaluate(ytest,ypred)
 
 #Mean absolute error: 0.12509324324324328
--- Lasso.py
+++ Lasso.py
@@ -1,4 +1,11 @@
-from sklearn import metrics
+import numpy as np
+import pandas as pd
+from sklearn import metrics, linear_model
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+
 def output_evaluate(target_value, predict_value):
     mean_absolute_error = metrics.mean_absolute_error(target_value, predict_value)
     mean_square_error = metrics.mean_squared_error(target_value, predict_value)
@@ -10,8 +17,6 @@ def output_evaluate(target_value, predict_value):
     print('Coefficient of determination', coefficient_of_determination)
 
 #read data
-import numpy as np
-import pandas as pd
 df = pd.read_csv('updated_data.csv', header=0)
 #print(df.head()) #return first 5 rows
 X1_addressCode = df.iloc[:,2]
@@ -21,20 +26,17 @@ X4_type = df.iloc[:,5]
 X = np.column_stack((X1_addressCode,X2_BedNum,X3_BathNum,X4_type))
 y = df.iloc[:,1]/1000 #rent is mostly four digits, to make value smaller and easier to read, divided price by 1000
 
-#split data into 80% for training and 20% for testing
-from sklearn.model_selection import train_test_split
+#split data into 90% for training and 10% for testing
 Xtrain, Xtest, ytrain, ytest = train_test_split(X,y,test_size=0.1,shuffle=True)
 
 #train training data using F-fold cross validation and Lasso regression model
 Ci_range = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 30, 40, 50, 60 ,70]
-from sklearn.model_selection import KFold
 kf = KFold(n_splits=5,shuffle=True)
 mean_error_mse = []
 std_error_mse = []
 mean_error_r2 = []
 std_error_r2 = []
 for Ci in Ci_range:
-    from sklearn import linear_model
     alpha = 1/(2*Ci)
     model = linear_model.Lasso(alpha=alpha)
     temp_mse = []
@@ -42,14 +44,12 @@ for Ci in Ci_range:
     for train, test in kf.split(Xtrain,ytrain):
         model.fit(Xtrain[train], ytrain.to_numpy()[train])
         ypred = model.predict(Xtrain[test])
-        from sklearn.metrics import mean_squared_error,r2_score
         temp_mse.append(mean_squared_error(ytrain.to_numpy()[test],ypred))
         temp_r2.append(r2_score(ytrain.to_numpy()[test],ypred))
     mean_error_mse.append(np.array(temp_mse).mean())
     std_error_mse.append(np.array(temp_mse).std())
     mean_error_r2.append(np.array(temp_r2).mean())
     std_error_r2.append(np.array(temp_r2).std())
-import matplotlib.pyplot as plt
 plt.errorbar(Ci_range, mean_error_mse, yerr=std_error_mse, linewidth=3)
 plt.ylabel('Mean square error')
 plt.xlabel('Ci')
@@ -85,3 +85,68 @@ output_evaluate(ytest,ypred)
 #Mean squared error: 0.20432194909347393
 #Root mean squared error: 0.45201985475582146
 #Coefficient of determination 0.7244870491812634
+
+#use polynomial features to train the model
+poly = PolynomialFeatures(2)
+polyX = poly.fit_transform(X)
+
+#split data into 80% for training and 20% for testing
+Xtrain, Xtest, ytrain, ytest = train_test_split(polyX,y,test_size=0.1,shuffle=True)
+
+#train training data using F-fold cross validation and Lasso regression model
+kf = KFold(n_splits=5,shuffle=True)
+mean_error_mse = []
+std_error_mse = []
+mean_error_r2 = []
+std_error_r2 = []
+for Ci in Ci_range:
+    alpha = 1/(2*Ci)
+    model = linear_model.Lasso(alpha=alpha)
+    temp_mse = []
+    temp_r2 = []
+    for train, test in kf.split(Xtrain,ytrain):
+        model.fit(Xtrain[train], ytrain.to_numpy()[train])
+        ypred = model.predict(Xtrain[test])
+        temp_mse.append(mean_squared_error(ytrain.to_numpy()[test],ypred))
+        temp_r2.append(r2_score(ytrain.to_numpy()[test],ypred))
+    mean_error_mse.append(np.array(temp_mse).mean())
+    std_error_mse.append(np.array(temp_mse).std())
+    mean_error_r2.append(np.array(temp_r2).mean())
+    std_error_r2.append(np.array(temp_r2).std())
+plt.errorbar(Ci_range, mean_error_mse, yerr=std_error_mse, linewidth=3)
+plt.ylabel('Mean square error')
+plt.xlabel('Ci')
+plt.show()
+
+plt.errorbar(Ci_range, mean_error_r2, yerr=std_error_r2, linewidth=3)
+plt.ylabel('R2 score')
+plt.xlabel('Ci')
+plt.show()
+
+C = 5 #30
+alpha = 1/(2*C)
+model = linear_model.Lasso(alpha=alpha)
+model.fit(Xtrain,ytrain)
+ypred = model.predict(Xtest)
+print(model.coef_)
+#[ 0.         -0.          0.          0.         -0.         -0.0015977
+#  0.01867471  0.05731545 -0.03286495  0.04038638  0.          0.
+#  0.         -0.         -0.        ]
+
+#on the 45 degree line, the predict value is same as the original value
+#above the line, predicted value is larger and below the line, predicted value is smaller
+plt.scatter(ytest, ypred)
+plt.xlim(1,7)
+plt.xlabel("ytest")
+plt.ylim(1,7)
+plt.ylabel("ypred")
+a=np.array([1,2,3,4,5,6,7])
+plt.plot(a,a,'r')
+plt.show()
+
+output_evaluate(ytest,ypred)
+
+#Mean absolute error: 0.2794995751172532
+#Mean squared error: 0.12534563656857592
+#Root mean squared error: 0.35404185708553715
+#Coefficient of determination 0.8071995603109717
